8af18eb6e2e6d8dc0127dfa990bc86becde67849
vinod <vinod@r081247.rgukt.in>
2021-11-05 21:49:11 +0530

    created components folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,71 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './components/public/public/public.component';
+import { PublicHomeComponent } from './components/public/public-home/public-home.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { CustomerComponent } from './components/customer/customer/customer.component';
+import { CustomerHomeComponent } from './components/customer/customer-home/customer-home.component';
+import { DeleteAccountComponent } from './components/customer/delete-account/delete-account.component';
+import { NewTransactionComponent } from './components/customer/new-transaction/new-transaction.component';
+import { TransactionHistoryComponent } from './components/customer/transaction-history/transaction-history.component';
+import { UpdatePasswordComponent } from './components/customer/update-password/update-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'public',
+    component: PublicComponent,
+    children: [
+      {
+        path: '',
+        component: PublicHomeComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ]
+  },
+  {
+    path: 'customer',
+    component: CustomerComponent,
+    children: [
+      {
+        path: '',
+        component: CustomerHomeComponent,
+      },
+      {
+        path: 'updatePassword',
+        component: UpdatePasswordComponent,
+      },
+      {
+        path: 'deleteAccount',
+        component: DeleteAccountComponent,
+      },
+      {
+        path: 'newTransaction',
+        component: NewTransactionComponent,
+      },
+      {
+        path: 'transactionHistory',
+        component: TransactionHistoryComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ]
+  },
+  { path: '**', redirectTo: "public"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PublicComponent } from './components/public/public/public.component';
+import { PublicHomeComponent } from './components/public/public-home/public-home.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { CustomerHomeComponent } from './components/customer/customer-home/customer-home.component';
+import { DeleteAccountComponent } from './components/customer/delete-account/delete-account.component';
+import { NewTransactionComponent } from './components/customer/new-transaction/new-transaction.component';
+import { TransactionHistoryComponent } from './components/customer/transaction-history/transaction-history.component';
+import { UpdatePasswordComponent } from './components/customer/update-password/update-password.component';
+import { CustomerComponent } from './components/customer/customer/customer.component';
+import { PublicHeaderComponent } from './components/public/public-header/public-header.component';
+import { CustomerHeaderComponent } from './components/customer/customer-header/customer-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    PublicHeaderComponent,
+    PublicHomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    CustomerComponent,
+    CustomerHeaderComponent,
+    CustomerHomeComponent,
+    DeleteAccountComponent,
+    NewTransactionComponent,
+    TransactionHistoryComponent,
+    UpdatePasswordComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
